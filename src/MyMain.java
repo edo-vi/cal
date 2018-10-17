0e106709a37008b63dc28d83d84289fbe51ef315
Edoardo Z <edovi@icloud.com>
2018-10-17 22:27:51 +0200

    Now Tuple.fromString handles no separator and end of line separator cases

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,5 +1,5 @@
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println(Tuple.fromString("anno=2", 6,'='));
+        System.out.println(Tuple.fromString("anno=", 5,'='));
     }
 }
--- src/Tuple.java
+++ src/Tuple.java
@@ -17,15 +17,15 @@ public class Tuple<T,W> {
     public static Tuple<String, String> fromString(String s, int length, char separator) {
         String one = "";
         String two = "";
-        int i = 0;
-        char val = s.charAt(i);
-
-        while(i<length && val != separator) {
-            i++;
-            val= s.charAt(i);
+        int i = s.indexOf(separator);;
+        if (i<0) {
+            throw new Error("No separator");
         }
+
         one+=s.substring(0,i);
-        two+=s.substring(i+1,length);
+        if (i<length-1) {
+            two+=s.substring(i+1,length);
+        }
 
         return new Tuple<>(one,two);
     }
