99e76d23c05e69bf79ae43ea087e3cda802acbe2
Edoardo Z <edovi@icloud.com>
2018-10-19 15:33:51 +0200

    Change default params to GetQuery instead of Option to not mix semantic

--- src/Parser.java
+++ src/Parser.java
@@ -1,24 +1,24 @@
 import java.util.ArrayList;
 
 public class Parser {
-    private final Option[] base = new Option[] {
-        new Option("form-type=corso"),
-        new Option("list=0"),
-        new Option("anno=2018"),
-        new Option("corso=420"), //Informatica
-        new Option("anno2=999%7C2"),
-        new Option("anno2_multi=999%7C2"),
-        new Option("visualizzazione_orario=cal"),
-        new Option("date=29-10-2018"), //todo change
-        new Option("periodo_didattico="),
-        new Option("_lang=it"),
-        new Option("txtaa=2018/2019"), //periodo scolastico
-        new Option("txtcorso=Laurea%20in%20informatica%20(Corsi%20di%20laurea)"),
-        new Option("txtanno=2%20anno%20-%20Unico"),
-        new Option("docente="),
-        new Option("attivita="),
-        new Option("txtdocente="),
-        new Option("txtattività=")
+    private final GetQuery[] base = new GetQuery[] {
+        new GetQuery("form-type","corso"),
+        new GetQuery("list","0"),
+        new GetQuery("anno","2018"),
+        new GetQuery("corso","420"), //Informatica
+        new GetQuery("anno2","999%7C2"),
+        new GetQuery("anno2_multi","999%7C2"),
+        new GetQuery("visualizzazione_orario","cal"),
+        new GetQuery("date","29-10-2018"), //todo change
+        //new GetQuery("periodo_didattico",""),
+        new GetQuery("_lang","it"),
+        new GetQuery("txtaa","2018/2019"), //periodo scolastico
+        new GetQuery("txtcorso","Laurea%20in%20informatica%20(Corsi%20di%20laurea)"),
+        new GetQuery("txtanno","2%20anno%20-%20Unico"),
+        //new GetQuery("docente",""),
+        //new GetQuery("attivita",""),
+        //new GetQuery("txtdocente",""),
+        //new GetQuery("txtattività","")
     };
 
     Parser(String[] args){
@@ -28,6 +28,15 @@ public class Parser {
             baseArgs.add(new Option(s));
         }
 
+
         // add current week as default
+
+    }
+}
+
+
+final class GetQuery extends Tuple<String,String> {
+    GetQuery(String a, String b) {
+        super(a,b);
     }
 }
